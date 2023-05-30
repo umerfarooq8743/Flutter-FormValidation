aaa2f4b186eb7aae721fc2b53b63899569be47bb
umer <s.m.umerfarooq8743@gmail.com>
2023-05-30 10:06:35 +0500

    first commit

---
+++ lib/form.dart
@@ -0,0 +1,135 @@
+import 'package:flutter/material.dart';
+
+class form extends StatefulWidget {
+  const form({super.key});
+
+  @override
+  State<form> createState() => _formState();
+}
+
+class _formState extends State<form> {
+  final _formfield = GlobalKey<FormState>();
+  final emailController = TextEditingController();
+  final passController = TextEditingController();
+  bool passToggle = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Form Validation"),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 60),
+        child: Form(
+            key: _formfield,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 70,
+                ),
+                TextFormField(
+                  autovalidateMode: AutovalidateMode.always,
+                  keyboardType: TextInputType.emailAddress,
+                  controller: emailController,
+                  decoration: InputDecoration(
+                      labelText: "Email",
+                      border: OutlineInputBorder(),
+                      prefixIcon: Icon(Icons.email)),
+                  validator: (value) {
+                    bool emailValid = RegExp(
+                            r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                        .hasMatch(value!);
+                    if (value.isEmpty) {
+                      return "Enter Email";
+                    } else if (!emailValid) {
+                      return "Enter Valid Email";
+                    }
+                  },
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  keyboardType: TextInputType.emailAddress,
+                  // autovalidateMode: AutovalidateMode.always,
+                  controller: passController,
+                  obscureText: passToggle,
+                  decoration: InputDecoration(
+                    labelText: "Passsword",
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.lock),
+                    suffix: InkWell(
+                      onTap: () {
+                        setState(() {
+                          passToggle = !passToggle;
+                        });
+                      },
+                      child: Icon(
+                          passToggle ? Icons.visibility : Icons.visibility_off),
+                    ),
+                  ),
+                  validator: (value) {
+                    bool passValid = RegExp(
+                            r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$')
+                        .hasMatch(value!);
+                    if (value.isEmpty) {
+                      return "Enter password";
+                    } else if (passController.text.length < 6) {
+                      return "Enter Valid password";
+                    } else if (!passValid) {
+                      return "Enter Valid password";
+                    }
+                  },
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                InkWell(
+                  onTap: () {
+                    if (_formfield.currentState!.validate()) {
+                      emailController.clear();
+                      passController.clear();
+                    }
+                  },
+                  child: Container(
+                    height: 50,
+                    decoration: BoxDecoration(
+                      color: Colors.indigo,
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    child: Center(
+                      child: Text(
+                        "Log In",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Already have Account?"),
+                    TextButton(
+                      onPressed: () {
+                        print('object');
+                      },
+                      child: Text("Sign Up!"),
+                    )
+                  ],
+                )
+              ],
+            )),
+      ),
+    );
+  }
+}
